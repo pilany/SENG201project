800ca3b7c99dd6052166050fd718b1695b4d49b0
pilany <55814311+pilany@users.noreply.github.com>
2020-05-19 15:44:40 +1200

    Delete InsufficientException.java

--- InsufficientException.java
+++
@@ -1,8 +0,0 @@
-package project1;
-/** create a InsufficientException class extends the class  of IllegalStateException*/
-public class InsufficientException extends IllegalStateException{
-	public InsufficientException(){}
-	public InsufficientException(String message) {
-		super(message);
-	}
-}
